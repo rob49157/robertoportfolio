4341f792f3fc130d21db6b63913b4c64225c0851
rob49157 <robertopaipo@hotmail.com>
2022-11-04 20:58:42 -0400

    home

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="Home">
+      <div className="about">
+        <h2>Hi, My Name is Roberto</h2>
+        <div className="prompt">
+          <p> A software developer with a passion for learrning and creating</p>
+
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2>Front-End</h2>
+            <span>ReactJS, HTML, CSS, React Native, NPM, Bootstrap</span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>NodeJS, Django, Rails, ExpressJS, GraphQl, ApolloServer, MySQL, MongoDB, REST API</span>
+          </li>
+          <li className="item">
+            <h2>Lenguages</h2>
+            <span>JavaScript, Java, Python, Ruby</span>
+          </li>
+        </ol>
+      </div>
+    </div>
   )
 }
 
