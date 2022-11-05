3e7a9767ce1009270b4d06271019bd699a13dbf4
rob49157 <robertopaipo@hotmail.com>
2022-11-04 22:39:15 -0400

    projects

---
+++ my-app/src/helpers/ProjectLists.js
@@ -0,0 +1,50 @@
+import ProjectItem from "../components/ProjectItem"
+import Doctoreview from "../assets/doctorpage.PNG"
+import Maxpage from "../assets/maxpage.PNG"
+import Marketapp from "../assets/reactcrypto.PNG"
+import BookSearch from "../assets/booksearch.PNG"
+import Calendar from "../assets/redcalendar.PNG"
+import bikeadventure from "../assets/bikeadventure.png"
+import NoteTaker from "../assets/notetakerpic.PNG"
+import Blog from "../assets/techblogpic.PNG"
+import Password from "../assets/passgenpic.PNG"
+
+
+export const projectList = {
+    {
+        name:"Doctor Review",
+        image:Doctoreview,
+    },
+    {
+        name:"Tarot Reading",
+        image:Maxpage,
+    },
+    {
+        name:"Marketapp",
+        image:Marketapp,
+    },
+    {
+        name:"Book Search",
+        image:BookSearch,
+    },
+    {
+        name:"Calendar",
+        image:Calendar,
+    },
+    {
+        name:"Bike Adventure",
+        image:Doctoreview,
+    },
+    {
+        name:"Note Taker",
+        image:NoteTaker,
+    },
+    {
+        name:"Tech Blog",
+        image:Blog,
+    },
+    {
+        name:"Password Generator",
+        image:Password,
+    },
+}
--- my-app/src/pages/Projects.js
+++ my-app/src/pages/Projects.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import ProjectItem from "../components/ProjectItem";
+import Doctoreview from "../assets/doctorpage.PNG";
+import Maxpage from "../assets/maxpage.PNG";
+import Marketapp from "../assets/reactcrypto.PNG";
+import BookSearch from "../assets/booksearch.PNG";
+import Calendar from "../assets/redcalendar.PNG";
+import bikeadventure from "../assets/bikeadventure.png";
+import NoteTaker from "../assets/notetakerpic.PNG";
+import Blog from "../assets/techblogpic.PNG";
+import Password from "../assets/passgenpic.PNG";
+import { projectList } from "../helpers/ProjectLists";
+
+import "../styles/Project.css";
 
 function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="projects">
+      <h1> My Projects</h1>
+      <div className="projectList">
+        {projectList.map((project) => {
+          return <ProjectItem name={project.name} image={project.image} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
